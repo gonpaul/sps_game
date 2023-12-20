99dc307a3eac90680392728f22926d142581e298
gonpaul <pavelgoncharov56@gmail.com>
2023-12-20 11:31:04 +0300

    added two state properties for counting the score

--- index.js
+++ index.js
@@ -15,36 +15,51 @@ const gameRound = (moves) => {
   return [usersMove, machinesMove];
 };
 
-const evalWinner = (usersMove, machinesMove) => {
+const evalWinner = (usersMove, machinesMove, state) => {
   const isEqual = (usersMove === machinesMove)
-  if (isEqual) { return 'Результат: Ничья' }
+  if (isEqual) { 
+    state.usersPoints += 1;
+    state.machinesPoints += 1; 
+    return 'Результат: Ничья'
+  }
   else if (usersMove === 'Камень' && machinesMove === 'Ножницы') {
+    state.usersPoints += 1;
     return 'Результат: Вы победили! Камень ломает ножницы. \n'
   }
   else if (usersMove === 'Ножницы' && machinesMove === 'Бумага') {
+    state.usersPoints += 1;
     return 'Результат: Вы победили! Ножницы режут бумагу. \n'
   }
   else if (usersMove === 'Бумага' && machinesMove === 'Камень') {
+    state.usersPoints += 1;
     return 'Результат: Вы победили! Бумага накрывает камень. \n'
   }
   else if (machinesMove === 'Камень' && usersMove === 'Ножницы') {
+    state.machinesPoints += 1;
     return 'Результат: Вы проиграли! Камень ломает ножницы. \n'
   }
   else if (machinesMove === 'Ножницы' && usersMove === 'Бумага') {
+    state.machinesPoints += 1;
     return 'Результат: Вы проиграли! Ножницы режут бумагу. \n'
   }
   else if (machinesMove === 'Бумага' && usersMove === 'Камень') {
+    state.machinesPoints += 1;
     return 'Результат: Вы проиграли! Бумага накрывает камень. \n'
   }
 };
 
 const spsGame = () => {
+  const state = {
+    usersPoints: 0,
+    machinesPoints: 0
+  };
   console.log('Добро пожаловать в игру "Камень, ножницы, бумага"!');
   const moves = ['Камень', 'Ножницы', 'Бумага'];
   let shouldContinue = true;
   while (shouldContinue) {
     const [usersMove, machinesMove] = gameRound(moves);
-    console.log(evalWinner(usersMove, machinesMove));
+    console.log(evalWinner(usersMove, machinesMove, state));
+    console.log(`Счёт: компьютер ${state.machinesPoints} vs вы ${state.usersPoints}`)
     const oneMore = readlineSync.question('Хотите сыграть ещё раз? (да/нет): ').toLowerCase();
     shouldContinue = (oneMore === 'да') ? true : false;
     if (!shouldContinue) {
